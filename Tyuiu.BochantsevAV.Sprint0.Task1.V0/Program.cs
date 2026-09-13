cda420a723b3d227476f23ef2b0c4d87a2d4a22d
BochantsevArtem <bochantsev.artem000@gmail.com>
2026-09-13 23:55:47 +0500

    Task1

--- Tyuiu.BochantsevAV.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.BochantsevAV.Sprint0.Task1.V0/Program.cs
@@ -1,14 +1,19 @@
 ﻿namespace Tyuiu.BochantsevAV.Sprint0.Task1.V0
+   
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
             string name = "Артем";
             int age = 18;
-            Console.WriteLine("Здравствуйте меня зовут " + name + ", мне " + age + " лет!");
-            Console.ReadKey();
+
+            Console.WriteLine( "Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+      
+                Console.ReadKey();
+
         }
     }
 }

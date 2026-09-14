47df26a35a8815e9cd0cc475ea961716f897f6b6
BochantsevArtem <bochantsev.artem000@gmail.com>
2026-09-14 18:31:12 +0500

    Task1

--- Tyuiu.BochantsevAV.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.BochantsevAV.Sprint0.Task1.V0/Program.cs
@@ -9,6 +9,7 @@
             string name = "Артем";
             int age = 18;
             Console.WriteLine( "Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+            
             Console.ReadKey();
 
         }

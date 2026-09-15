47694e362c821ce2534a9410d8290c937d5e87d5
BochantsevArtem <bochantsev.artem000@gmail.com>
2026-09-15 18:42:32 +0500

    Task5

---
+++ Tyuiu.BochantsevAV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.BochantsevAV.Sprint0.Task2.V0.Lib;
+
+
+
+namespace Tyuiu.BochantsevAV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Артем";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Артем", res);
+        }
+    }
+}
